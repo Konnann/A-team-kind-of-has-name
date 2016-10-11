5813f415215a84959711de3c8223ccb32c590ab8
Konan <j.vasileva@abv.bg>
2016-10-11 19:24:39 +0300

    Small additions and changes
    added hero, sprites, added background, centered game

--- BubbleTrouble/JavaScript/mainRender.js
+++ BubbleTrouble/JavaScript/mainRender.js
@@ -1,7 +1,11 @@
 //call all renders from classes here
 var ctx = document.getElementById("canvas").getContext("2d");
 function render(){
-    ctx.clearRect(0,0,800,600);
+    let canvas = new Image();
+    canvas.src = "./src/background.png";
+    ctx.drawImage(canvas, 0, 0);
+    //ctx.clearRect(0,0,800,600);
     arr.draw(ctx);
     ball.draw(ctx);
+    
 }
